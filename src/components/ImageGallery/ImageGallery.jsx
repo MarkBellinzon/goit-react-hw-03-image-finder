7dbf87977f21f65359be36d943e98d23080193f4
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-12-04 17:41:00 +0200

    img_galary

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,20 @@
-import { Component } from "react";
-import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
-import css from "./ImageGallery.module.css";
+import { Component } from 'react';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
 
 export class ImageGallery extends Component {
   render() {
     return (
       <ul className={css.ImageGallery}>
-        {this.props.images.map(({ id, webformatURL, largeImageURL, tags }) => (
+        {this.props.images.map(({ id, webformatURL, largeImageURL }) => (
           <ImageGalleryItem
             onModalClick={this.props.onModalClick}
             key={id}
             image={webformatURL}
             modalImage={largeImageURL}
-            description={tags.split(",")[0]}
           />
         ))}
       </ul>
     );
   }
 }
-
-
